f74d3f204e2652baa938c06d4c1b80e7908acdc1
Pavel Mezencev <mezencev24@mail.ru>
2022-07-06 23:26:49 +0300

    add 2 big O notation

--- 2_Big_O/big-O.js
+++ 2_Big_O/big-O.js
@@ -1,5 +1,37 @@
-function addOne(num) {
-  return num + 1;
+function stringReverse(str) {
+  const arr = [];
+  for (let i = str.length; i >= 0; i--) {
+    arr.push(str[i]);
+  }
+  return arr.join('');
 }
+console.log(stringReverse('Hello World!'));
 
-console.log(addOne(3));
+const calcNumSum = (num) => {
+  let sum = 0;
+
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+console.log(calcNumSum(3));
+
+const calcNumSum2 = (num) => {
+  return new Array(num)
+    .fill(1)
+    .map((item, i) => item * (i + 1))
+    .reduce((acc, cur) => acc + cur, 0);
+};
+console.log(calcNumSum2(100));
+
+const printAllPairs = (num) => {
+  for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= i; j++) {
+      console.log(`${j} / ${i}`);
+    }
+  }
+};
+
+printAllPairs(100);
