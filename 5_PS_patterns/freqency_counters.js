f7427c9b5a735d50862bd2d6aeb4d5b0470d034b
Pavel Mezencev <mezencev24@mail.ru>
2022-07-07 13:16:10 +0300

    add same fn

--- 5_PS_patterns/freqency_counters.js
+++ 5_PS_patterns/freqency_counters.js
@@ -1,16 +1,18 @@
 const same = (arr1, arr2) => {
-  const postions = [];
+  const positions = [];
 
   for (let i = 0; i < arr1.length; i++) {
-    const squared = arr1[i] ** 2;
+    const square = arr1[i] ** 2;
+
     for (let j = 0; j < arr2.length; j++) {
-      if (!postions.includes(j) && squared === arr2[j]) {
-        postions.push(j);
+      if (square === arr2[j] && !positions.includes(j)) {
+        positions.push(j);
+        break;
       }
     }
   }
 
-  return postions.length === arr1.length;
+  return arr1.length === positions.length;
 };
 
 console.log(same([1, 2, 3], [4, 1, 9]));
